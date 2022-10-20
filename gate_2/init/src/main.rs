236de5a68389951215c95d03ea6a944dbf63c454
jerrymusaga <musagatechnology@gmail.com>
2022-10-20 22:36:37 +0100

    structs impl

--- gate_2/init/src/main.rs
+++ gate_2/init/src/main.rs
@@ -5,6 +5,16 @@ struct User {
     active: bool
 }
 
+impl User {
+    fn new(name: String, age: u32, active: bool) -> Self {
+        Self {
+            name,
+            age,
+            active
+        }
+    }
+}
+
 fn set_user() -> User {
     User {
         name: String::from("Henry"),
@@ -23,4 +33,9 @@ fn main() {
 
     let henry = set_user();
     println!("{:?}", henry);
+
+    let emma = User::new("emma".to_string(), 22, false);
+    let juliet = User::new(String::from("juliet"), 25, true);
+
+    println!("{:?}->{:?}", emma, juliet);
 }
